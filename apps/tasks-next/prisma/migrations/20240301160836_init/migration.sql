d0531e417762d7b78bb2f6e9a961eca70eb36d67
Artem Belov <otsec.ru@gmail.com>
2024-03-03 17:07:37 +0100

    Remove NextJs tasks service

--- apps/tasks-next/prisma/migrations/20240301160836_init/migration.sql
+++
@@ -1,7 +0,0 @@
--- CreateTable
-CREATE TABLE "Task" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "userId" TEXT NOT NULL,
-    "name" TEXT NOT NULL,
-    CONSTRAINT "Task_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE CASCADE ON UPDATE CASCADE
-);
